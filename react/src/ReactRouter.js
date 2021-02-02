7ab4983721e5df93c18721a3c3891b05d1036ba7
mackenziewritescode <mackenzie.charlton@gmail.com>
2021-02-02 18:44:20 -0500

    updated drum machine

--- react/src/ReactRouter.js
+++ react/src/ReactRouter.js
@@ -1,24 +1,23 @@
-import React from "react"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-import Portfolio from "./portfolio/Portfolio"
-import Calculator from "./calculator/Calculator"
-import DrumMachine from "./drum-machine/DrumMachine"
-import Markdown from "./markdown-app/Markdown"
-import PhotoSearch from "./photo-search/PhotoSearch"
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Portfolio from "./portfolio/components/Portfolio";
+import Calculator from "./calculator/components/Calculator";
+import DrumMachine from "./drum-machine/components/DrumMachine";
+import Markdown from "./markdown-app/Markdown";
+import PhotoSearch from "./photo-search/PhotoSearch";
 
 function ReactRouter() {
-
-    return (
-        <Router>
-            <Switch>
-                <Route path="/" exact component={Portfolio} />
-                <Route path="/calculator" component={Calculator} />
-                <Route path="/drum-machine" component={DrumMachine} />
-                <Route path="/markdown-app" component={Markdown} />
-                <Route path="/photo-search" component={PhotoSearch} />
-            </Switch>
-        </Router>
-    )
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Portfolio} />
+        <Route path="/calculator" component={Calculator} />
+        <Route path="/drum-machine" component={DrumMachine} />
+        <Route path="/markdown-app" component={Markdown} />
+        <Route path="/photo-search" component={PhotoSearch} />
+      </Switch>
+    </Router>
+  );
 }
 
-export default ReactRouter
+export default ReactRouter;
